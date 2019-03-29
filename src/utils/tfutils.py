b1c51490f670b111431f189e447a43d34e58e455
Miltos Allamanis <miallama@microsoft.com>
2019-03-29 19:10:59 +0000

    Implement BPE for all sequence models.

--- src/utils/tfutils.py
+++ src/utils/tfutils.py
@@ -1,4 +1,4 @@
-from typing import List, Tuple, Dict, Any, Optional
+from typing import List, Tuple, Dict, Any, Optional, Union
 
 import numpy as np
 import tensorflow as tf
@@ -6,10 +6,12 @@ from tensorflow.python.ops.init_ops import Initializer
 
 from dpu_utils.mlutils import Vocabulary
 
+from utils.bpevocabulary import BpeVocabulary
+
 BIG_NUMBER = 1e7
 
 
-def convert_and_pad_token_sequence(token_vocab: Vocabulary,
+def convert_and_pad_token_sequence(token_vocab: Union[Vocabulary, BpeVocabulary],
                                    token_sequence: List[str],
                                    output_tensor_size: int,
                                    pad_from_left: bool = False) \
@@ -18,7 +20,7 @@ def convert_and_pad_token_sequence(token_vocab: Vocabulary,
     Tensorise token sequence with padding; returning a mask for used elements as well.
 
     Args:
-        token_vocab: Vocabulary to use. We assume that token_vocab[0] is the padding symbol.
+        token_vocab: Vocabulary or BPE encoder to use. We assume that token_vocab[0] is the padding symbol.
         token_sequence: List of tokens in string form
         output_tensor_size: Size of the resulting tensor (i.e., length up which we pad / down to which we truncate.
         pad_from_left: Indicate if we are padding/truncating on the left side of string. [Default: False]
@@ -27,6 +29,11 @@ def convert_and_pad_token_sequence(token_vocab: Vocabulary,
         Pair of numpy arrays. First is the actual tensorised token sequence, the second is a masking tensor
         that is 1.0 for those token indices that are actually used.
     """
+    if isinstance(token_vocab, BpeVocabulary):
+        token_ids = np.array(list(token_vocab.transform([token_sequence], fixed_length=output_tensor_size))[0])
+        token_mask = np.array([1 if token_ids[i] > 0 else 0 for i in range(len(token_ids))])
+        return token_ids, token_mask
+
     if pad_from_left:
         token_sequence = token_sequence[-output_tensor_size:]
     else:
