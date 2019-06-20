f6beff96ebd35e1ccf30524b5e227554c7e97c88
Ho-Hsiang Wu <hohsiangwu@users.noreply.github.com>
2019-06-20 11:37:22 -0400

    Update scripts and baseline predictions (#64)
    * Update scripts and baseline predictions
    
    * Remove several columns in the prediction file
    
    * Wire up with W&B model load
    
    * Update param
    
    * Submit prediction file to W&B
    
    * Attempt to fix mypy error
    
    * Revert "Attempt to fix mypy error"
    
    This reverts commit f103e6587363c42fcbae2c91814b0567821ca786.
    
    * Check predict.py file

--- src/predict.py
+++ src/predict.py
@@ -1,11 +1,14 @@
 """
 Usage:
-    predict.py LANGUAGE MODEL_FILE PREDICTION_FILE
+    predict.py [options] PREDICTION_FILE
 
-> python predict.py python ../resources/saved_models/*.pkl.gz ../resources/python_predictions.csv
+> python predict.py --wandb-run github/codesearchnet/xxxxxxxx ../resources/model_predictions.csv
+> python predict.py --model-file ../resources/saved_models/*.pkl.gz ../resources/model_predictions.csv
 
 Options:
     -h --help                        Show this screen.
+    --wandb-run PATH                 W&B run id.
+    --model-file PATH                Local model path.
 
 """
 import pickle
@@ -16,6 +19,7 @@ from docopt import docopt
 from dpu_utils.utils import RichPath
 import pandas as pd
 from tqdm import tqdm
+import wandb
 
 from dataextraction.python.parse_python_data import tokenize_docstring_from_string
 import model_restore_helper
@@ -34,29 +38,43 @@ if __name__ == '__main__':
     queries = pd.read_csv('../resources/queries.csv')
     queries = list(queries['query'].values)
 
-    definitions = pickle.load(open('../resources/data/{}_dedupe_definitions_v2.pkl'.format(args['LANGUAGE']), 'rb'))
+    run_id = None
+    if args.get('--wandb-run', None):
+        api = wandb.Api()
+        run = api.run(args['--wandb-run'])
+        model_file = [f for f in run.files() if f.name.endswith('gz')][0].download(replace=True)
+        local_model_path = model_file.name
+        run_id = args['--wandb-run'].split('/')[-1]
+    elif args.get('--model-file', None):
+        local_model_path = args['--model-file']
+    else:
+        raise Exception('Either --wandb-run or --model-file needs to be set')
 
-    model_path = RichPath.create(args['MODEL_FILE'], None)
+    model_path = RichPath.create(local_model_path, None)
     model = model_restore_helper.restore(
         path=model_path,
         is_train=False,
         hyper_overrides={})
-    model_name = re.search(".*'(.*)'.*", str(type(model))).group(1).split('.')[-1]
-
-    indexes = [{'code_tokens': d['function_tokens'], 'language': d['language']} for d in tqdm(definitions)]
-    code_representations = model.get_code_representations(indexes)
-
-    indices = AnnoyIndex(code_representations[0].shape[0])
-    for index, vector in tqdm(enumerate(code_representations)):
-        if vector is not None:
-            indices.add_item(index, vector)
-    indices.build(10)
 
     predictions = []
-    for query in queries:
-        for idx, _ in zip(*query_model(query, model, indices, args['LANGUAGE'])):
-            predictions.append((query, model_name, args['LANGUAGE'], definitions[idx]['identifier'],
-                                definitions[idx]['function'], definitions[idx]['docstring_summary'], definitions[idx]['url']))
+    for language in ('python', 'go', 'javascript', 'java', 'php', 'ruby'):
+        definitions = pickle.load(open('../resources/data/{}_dedupe_definitions_v2.pkl'.format(language), 'rb'))
+        indexes = [{'code_tokens': d['function_tokens'], 'language': d['language']} for d in tqdm(definitions)]
+        code_representations = model.get_code_representations(indexes)
 
-    df = pd.DataFrame(predictions, columns=['query', 'model_name', 'language', 'identifier', 'function', 'docstring_summary', 'url'])
+        indices = AnnoyIndex(code_representations[0].shape[0])
+        for index, vector in tqdm(enumerate(code_representations)):
+            if vector is not None:
+                indices.add_item(index, vector)
+        indices.build(10)
+
+        for query in queries:
+            for idx, _ in zip(*query_model(query, model, indices, language)):
+                predictions.append((query, language, definitions[idx]['identifier'], definitions[idx]['url']))
+
+    df = pd.DataFrame(predictions, columns=['query', 'language', 'identifier', 'url'])
     df.to_csv(args['PREDICTION_FILE'], index=False)
+
+    if run_id:
+        wandb.init(id=run_id, resume=True)
+        wandb.save(args['PREDICTION_FILE'])
